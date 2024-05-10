dd793625b5cfe2635bc358517af83c0935ab0b20
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-10 22:41:44 +0300

    tailwind added

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,7 +1,7 @@
 export const Home: React.FC = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
     </div>
   );
 };
