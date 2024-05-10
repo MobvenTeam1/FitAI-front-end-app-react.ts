32d243ba791ff4958bf0768515279fe770fbc030
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-10 22:47:02 +0300

    router updated

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import { NotFound404 } from "../pages/NotFound404";
 import { DashboardLayoutMain } from "../layouts/dashboard/DashboardLayoutMain";
 import { Home } from "../pages/dashboard/Home";
 import { Users } from "../pages/dashboard/Users";
+import { Login } from "../pages/auth/Login";
+import { Register } from "../pages/auth/Register";
 
 const routes = createBrowserRouter([
   {
+    path: "auth",
+    // element: <AuthGuard />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: (
+      <>
+        <Outlet />
+      </>
+    ),
     children: [
       {
         path: "/",
+        // element: <GuestGuard />,
         element: <DashboardLayoutMain />,
         children: [
           {
@@ -22,11 +45,12 @@ const routes = createBrowserRouter([
         ],
       },
 
-      {
-        path: "*",
-        element: <NotFound404 />,
-      },
+  
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound404 />,
   },
 ]);
 
