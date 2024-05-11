f17ae9af9000e07d439ef4b33263dffe047d2b63
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-11 15:13:51 +0300

    rhf validate and error added

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -8,7 +8,8 @@ type FormValues = {
 
 export const Login: React.FC = () => {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log("data", data);
@@ -16,7 +17,7 @@ export const Login: React.FC = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-8 p-6 bg-white rounded-lg shadow-md">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Login
@@ -27,7 +28,6 @@ export const Login: React.FC = () => {
           onSubmit={handleSubmit(onSubmit)}
           noValidate
         >
-          <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <label htmlFor="email-address" className="sr-only">
@@ -35,7 +35,6 @@ export const Login: React.FC = () => {
               </label>
               <input
                 id="email-address"
-                // name="email"
                 type="email"
                 {...register("email", {
                   required: {
@@ -46,10 +45,18 @@ export const Login: React.FC = () => {
                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
                     message: "Invalid email address",
                   },
+                  validate: {
+                    notAdmin: (fieldValue: string) =>
+                      fieldValue !== "admin@example.com" || "Enter valid email",
+                    notBlackedList: (fieldValue: string) =>
+                      !fieldValue.includes("blacklist") ||
+                      "This email is blacklisted",
+                  },
                 })}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500"
                 placeholder="Email address"
               />
+              <p className="text-red-500">{errors.email?.message}</p>
             </div>
             <div>
               <label htmlFor="password" className="sr-only">
@@ -57,7 +64,6 @@ export const Login: React.FC = () => {
               </label>
               <input
                 id="password"
-                // name="password"
                 type="password"
                 {...register("password", {
                   required: {
@@ -70,16 +76,17 @@ export const Login: React.FC = () => {
                   },
                 })}
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500"
                 placeholder="Password"
               />
             </div>
+            <p className="text-red-500">{errors.password?.message}</p>
           </div>
 
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:shadow-lg"
             >
               Login
             </button>
