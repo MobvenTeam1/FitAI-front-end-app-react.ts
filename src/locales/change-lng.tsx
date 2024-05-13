d21a7aeeb8c72fab2b740fe87ec64ac32a820bfe
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-13 11:45:29 +0300

    changle lng updated

--- src/locales/change-lng.tsx
+++ src/locales/change-lng.tsx
@@ -32,18 +32,31 @@ export const ChangeLng: FC = () => {
   }, [i18n, language]);
 
   return (
-    <div className="flex justify-center items-center bg-gray-800 p-1 rounded-md">
-      <select
-        value={language}
-        onChange={onClickLanguageChange}
-        className="px-2 py-1 bg-white text-gray-800 border border-gray-300 rounded-md shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-500"
-      >
-        {LANGUAGES.map((lang: Language) => (
-          <option key={lang.value} value={lang.value}>
-            {lang.label}
-          </option>
-        ))}
-      </select>
+    <div className="flex justify-center items-center bg-gray-800 rounded-md">
+      <div className="relative inline-flex">
+        <svg
+          className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 412 232"
+        >
+          <path
+            d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.763-9.763 25.592 0 35.355l181 181c9.763 9.763 25.592 9.763 35.355 0l181-181c9.762-9.763 9.762-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
+            fill="#648299"
+            fill-rule="nonzero"
+          />
+        </svg>
+        <select
+          value={language}
+          onChange={onClickLanguageChange}
+          className="border border-gray-300 rounded-md text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+        >
+          {LANGUAGES.map((lang: Language) => (
+            <option key={lang.value} value={lang.value}>
+              {lang.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
