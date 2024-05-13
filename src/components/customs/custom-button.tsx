c451c0cb37bf3e9783b94753db7321d30b9c40df
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-13 16:39:49 +0300

    custom button updated

--- src/components/customs/custom-button.tsx
+++ src/components/customs/custom-button.tsx
@@ -7,28 +7,24 @@ interface CustomButtonProps {
   variant: "outlined" | "contained" | "link";
 }
 
-const generateClassName = (variant: CustomButtonProps["variant"]) => {
-  const baseClassName =
-    "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white transition-colors duration-200";
+const baseClassName =
+  "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white transition-colors duration-200";
 
-  const variantClassNames = {
-    outlined:
-      " bg-transparent border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white",
-    contained:
-      " bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:shadow-lg",
-    link: " bg-transparent text-indigo-500 hover:text-indigo-600",
-  };
-
-  return baseClassName + (variantClassNames[variant] || "");
+const variantClassNames = {
+  outlined:
+    "bg-transparent border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white",
+  contained:
+    "bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:shadow-lg",
+  link: "bg-transparent text-indigo-500 hover:text-indigo-600",
 };
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   type = "button",
   label = "",
-  variant,
+  variant = "contained",
   ...props
 }) => {
-  const className = generateClassName(variant);
+    const className = `${baseClassName} ${variantClassNames[variant]}`;
 
   return (
     <button type={type} className={className} {...props}>
