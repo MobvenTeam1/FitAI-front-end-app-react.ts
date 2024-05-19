474f71a71da9e6b53c5b9e9a8555037cd346dd20
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-20 01:08:40 +0300

    rhfTextfield type password updated

--- src/components/hook-form/RHFTextfield.tsx
+++ src/components/hook-form/RHFTextfield.tsx
@@ -9,6 +9,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: string;
 }
 
+const PasswordToggleButton: FC<{ onClick: () => void }> = ({ onClick }) => (
+  <button
+    className="text-gray-500 absolute top-1/2 right-2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
+    type="button"
+    onClick={onClick}
+  >
+    <SvgColor width={24} height={24} src={`/icons/ic_password_eye.svg`} />
+  </button>
+);
+
 export const RHFTextfield: FC<InputProps> = ({
   name,
   label,
@@ -19,14 +29,15 @@ export const RHFTextfield: FC<InputProps> = ({
     register,
     formState: { errors },
   } = useFormContext();
-
   const error: FieldError | undefined = errors[name] as FieldError;
-
   const [inputType, setInputType] = useState(type);
 
-  const togglePasswordVisibility = () => {
-    setInputType(inputType === "password" ? "text" : "password");
-  };
+  const togglePasswordVisibility = () =>
+    setInputType((prevType) => (prevType === "password" ? "text" : "password"));
+
+  const inputClass = `border border-gray-200 rounded-lg px-4 py-5 w-full bg-gray-300 ${
+    inputType === "password" ? "font-black tracking-widest" : ""
+  }`;
 
   return (
     <div>
@@ -35,22 +46,12 @@ export const RHFTextfield: FC<InputProps> = ({
           id={name}
           type={inputType}
           placeholder={label}
-          className="border border-gray-200 rounded-lg px-4 py-5 w-full bg-gray-300"
+          className={inputClass}
           {...register(name)}
           {...props}
         />
         {type === "password" && (
-          <button
-            className="text-gray-500 absolute top-1/2 right-2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
-            type="button"
-            onClick={togglePasswordVisibility}
-          >
-            <SvgColor
-              width={24}
-              height={24}
-              src={`/icons/ic_password_eye.svg`}
-            />
-          </button>
+          <PasswordToggleButton onClick={togglePasswordVisibility} />
         )}
       </div>
       <ErrorMessage error={error} />
