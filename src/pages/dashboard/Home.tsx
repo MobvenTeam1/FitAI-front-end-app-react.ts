46f20678f1c62644ff8dba9dcd06a945afdedbcc
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-24 09:27:50 +0300

    home updated

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,9 +1,7 @@
 import { TemplateHookForm } from "../../sections/howToUse-Doc/example-template-react-hooh-form";
-import { UseDataUsage } from "../../sections/howToUse-Doc/useData-usage";
 export const Home: React.FC = () => {
   return (
     <div>
-      <UseDataUsage />
       <TemplateHookForm />
     </div>
   );
