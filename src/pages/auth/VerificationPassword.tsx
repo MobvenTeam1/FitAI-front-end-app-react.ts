bb7655f5e198dc2df5ee3898146f4e916a6826e4
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-28 14:24:28 +0300

    verification code updated

--- src/pages/auth/VerificationPassword.tsx
+++ src/pages/auth/VerificationPassword.tsx
@@ -1,78 +1,99 @@
-import { FormProvider, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { DevTool } from "@hookform/devtools";
-import * as yup from "yup";
-import { RHFTextfield } from "../../components/hook-form/RHFTextfield";
-import { RHFSubmitButton } from "../../components/hook-form/RHFSubmitButton";
 // import { RHFFormValues } from "../../components/hook-form/RHFFormValues";
 import { paths } from "../../routes/paths";
 import { AuthHeader } from "../../sections/auth/AuthHeader";
 import { AuthLink } from "../../sections/auth/AuthLink";
 import { useRouter } from "../../hooks/useRouter";
-
-export type FormValues = {
-  verificationCode: string;
-};
-
-const schema = yup.object().shape({
-  verificationCode: yup.string().required("Doğrulama kodu zorunlu"),
-});
-
-const defaultValues: FormValues = {
-  verificationCode: "1234",
-};
+import { useRef, useState } from "react";
+import { CustomButton } from "../../components/customs/custom-button";
 
 export const VerificationPassword: React.FC = () => {
   const router = useRouter();
-  const form = useForm<FormValues>({
-    defaultValues,
-    resolver: yupResolver(schema),
-  });
+  const [values, setValues] = useState(["", "", "", ""]);
+  const inputRefs = [
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+  ];
 
-  const { control, handleSubmit } = form;
-
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = () => {
     handlePush(`/${paths.auth.root}/${paths.auth.createNewPassword}`);
-    console.log(data);
   };
 
   const handlePush = (path: string) => {
     router.push(path);
   };
 
+  const handleInputChange = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+    if (/^\d$/.test(value)) {
+      const newValues = [...values];
+      newValues[index] = value;
+      setValues(newValues);
+      if (index < inputRefs.length - 1) {
+        inputRefs[index + 1].current?.focus();
+      }
+    }
+  };
+
+  const handleKeyDown = (
+    index: number,
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Backspace") {
+      const newValues = [...values];
+      if (newValues[index] === "" && index > 0) {
+        inputRefs[index - 1].current?.focus();
+      }
+      newValues[index] = "";
+      setValues(newValues);
+    }
+  };
+
   return (
     <>
       <div className="min-h-screen flex items-center justify-center">
-        <FormProvider {...form}>
-          <form
-            className="w-full px-20 flex flex-col gap-9"
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-          >
-            <AuthHeader
-              title="Doğrulama"
-              subtitle="E-posta adresinize gönderdiğimiz doğrulama kodunu girin."
-            />
+        <form
+          className="w-full px-20 flex flex-col gap-9"
+          onSubmit={onSubmit}
+          noValidate
+        >
+          <AuthHeader
+            title="Doğrulama"
+            subtitle="E-posta adresinize gönderdiğimiz doğrulama kodunu girin."
+          />
 
-            <div className="flex flex-col gap-6">
-              <RHFTextfield name="verificationCode" label="Doğrulama Kodu" />
-            </div>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <RHFSubmitButton label="Onayla" />
-              <AuthLink
-                title="Kod almadınız mı?"
-                rootText="Tekrar gönder"
-                path={`/${paths.auth.root}/${paths.auth.register}`}
+          <div className="flex justify-between gap-6">
+            {inputRefs.map((ref, index) => (
+              <input
+                key={index}
+                ref={ref}
+                value={values[index]}
+                maxLength={1}
+                onChange={(e) => handleInputChange(index, e)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                className={`w-full p-12 border rounded-lg text-center text-lg font-bold focus:outline-none focus:ring-1 focus:ring-green-500 focus:border-transparent focus:placeholder-transparent max-sm:p-1 ${
+                  values[index] ? "border-green-600" : "border-black-300"
+                }`}
               />
-            </div>
+            ))}
+          </div>
 
-            {/* <RHFFormValues /> */}
-          </form>
-        </FormProvider>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <CustomButton label="Doğrula" />
+            <AuthLink
+              title="Kod almadınız mı?"
+              rootText="Tekrar gönder"
+              path={`/${paths.auth.root}/${paths.auth.register}`}
+            />
+          </div>
+
+          {/* <RHFFormValues /> */}
+        </form>
       </div>
-
-      <DevTool control={control} />
     </>
   );
 };
