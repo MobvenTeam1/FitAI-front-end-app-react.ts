b823c125c6e63016554e376e546980f6a4c4cf92
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-30 17:33:04 +0300

    sign added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,30 @@ export const App: React.FC = () => {
     <>
       <AuthContextProvider>
         <RouterProvider router={routes} />
+        <a
+          className="text-xs text-gray-100 absolute top-0 right-0"
+          href="mailto:goktugdemirwebdev@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          goktugdemirwebdev@gmail.com
+        </a>
+        <a
+          className="text-xs text-gray-100 absolute top-4 right-0"
+          href="https://www.linkedin.com/in/goktugmuratdemir/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="text-xs text-gray-100 absolute top-8 right-0"
+          href="https://www.linkedin.com/in/goktugmuratdemir/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
       </AuthContextProvider>
     </>
   );
