36e76ed7e198beb2cf6758cd282d94782fe07af4
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-06-02 16:06:06 +0300

    fDate utils tr support addded

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,4 +1,5 @@
 import { format, getTime, formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
 
 // ----------------------------------------------------------------------
 
@@ -7,13 +8,13 @@ export const TodayDate = new Date();
 export function fDate(date: Date | string, newFormat?: string): string {
   const fm = newFormat || "dd MMM yyyy";
 
-  return date ? format(new Date(date), fm) : "";
+  return date ? format(new Date(date), fm, { locale: tr }) : "";
 }
 
 export function fDateTime(date: Date | string, newFormat?: string): string {
   const fm = newFormat || "dd MMM yyyy p";
 
-  return date ? format(new Date(date), fm) : "";
+  return date ? format(new Date(date), fm, { locale: tr }) : "";
 }
 
 export function fTimestamp(date: Date | string): number | string {
@@ -24,6 +25,7 @@ export function fToNow(date: Date | string): string {
   return date
     ? formatDistanceToNow(new Date(date), {
         addSuffix: true,
+        locale: tr,
       })
     : "";
 }
