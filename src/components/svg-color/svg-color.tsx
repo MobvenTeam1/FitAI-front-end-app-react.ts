bcef9bef31158efc7e343518a812da885331b033
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-06-12 17:21:01 +0300

    svg issue test 1.4

--- src/components/svg-color/svg-color.tsx
+++ src/components/svg-color/svg-color.tsx
@@ -8,22 +8,28 @@ interface SvgColorProps extends HTMLAttributes<HTMLSpanElement> {
   height?: number;
 }
 
+//
+
 const SvgColor = forwardRef<HTMLSpanElement, SvgColorProps>(
-  ({ src, style, width = 12, height = 12, ...other }, ref) => (
-    <span
-      ref={ref}
-      style={{
-        width,
-        height,
-        display: "inline-block",
-        backgroundColor: "currentColor",
-        mask: `url(${src}) no-repeat center / contain`,
-        WebkitMask: `url(${src}) no-repeat center / contain`,
-        ...style,
-      }}
-      {...other}
-    />
-  )
+  ({ src, style, width = 12, height = 12, ...other }, ref) => {
+    const source = new URL(`${src}`, import.meta.url).href;
+
+    return (
+      <span
+        ref={ref}
+        style={{
+          width,
+          height,
+          display: "inline-block",
+          backgroundColor: "currentColor",
+          mask: `url(${source}) no-repeat center / contain`,
+          WebkitMask: `url(${source}) no-repeat center / contain`,
+          ...style,
+        }}
+        {...other}
+      />
+    );
+  }
 );
 
 export default SvgColor;
